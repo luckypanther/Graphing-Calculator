badc7af2de413c1061685ffbd43d7202b551c4ed
Sreyas Adiraju <luckypanther720@gmail.com>
2018-02-16 22:22:32 -0500

    Adding basic webpage with canvas, CSS, and JS

---
+++ code.js
@@ -0,0 +1,12 @@
+let canvas
+let ctx
+
+function init(){
+  canvas = document.getElementById('canvas')
+  ctx = canvas.getContext('2d')
+  ctx.requestAnimationFrame(draw)
+}
+
+function draw(){
+  
+}
