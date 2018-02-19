d8b9455468dfa694749a493b788907f82df158b8
Sreyas Adiraju <luckypanther720@gmail.com>
2018-02-18 21:44:02 -0500

    Refined coordinate conversion, circle drawing, point drawing, and added basic functionality for function graphing. Currently graphing sine waveand still missing scaling of x and y axes.

--- code.js
+++ code.js
@@ -4,8 +4,12 @@ let pixels
 let width, height
 let fps = 60
 let loop = true
-let x_min = -400
-let x_max = 400
+let x_min = -10
+let x_max = 10
+let y_min = 10
+let y_max = 10
+let CIRCLE_FILL = "black"
+let POINT_RADIUS = 2
 
 function init(){
   canvas = document.getElementById("canvas")
@@ -19,10 +23,14 @@ function draw(){
   if(!loop){
     return
   }
-  for(let x = x_min; x <=x_max; x++){
-    let y = x*x
-    circle(x, y, 0.1, "#000000")
+  for(let i = 0; i < width; i++){
+    let x = convert_to_graph(i)
+    let y = f(x)
+    let p = convert_to_draw(x, y)
+    console.log(p)
+    point(p.x, p.y)
   }
+  noLoop()
 }
 
 function loadPixels(){
@@ -33,20 +41,38 @@ function setPixels(){
   ctx.putImageData(pixels, 0, 0, width, height);
 }
 
-function convert_coord(x, y){
+function f(x){
+  return 200*Math.sin(x/200)
+}
+
+function convert_to_draw(x, y){
   return {x: x+width/2, y: height/2-y}
 }
 
-function circle(x, y, r, fill){
-  ctx.fillStyle = fill
-  let p = convert_coord(x, y)
-  ctx.arc(p.x, p.y, r, 0, 2*Math.PI)
-  ctx.stroke()
+function convert_to_graph(x, y){
+  if(y){
+    return {x: x-width/2, y: height/2-y}
+  }
+  return x-width/2
+}
+
+function circle(x, y, r){
+  ctx.beginPath()
+  ctx.fillStyle = CIRCLE_FILL
+  ctx.arc(x, y, r, 0, 2*Math.PI)
+  ctx.fill()
+}
+
+function point(x, y){
+  circle(x, y, POINT_RADIUS)
 }
 
 function line(x1, y1, x2, y2){
   ctx.beginPath()
-  let p1 = convert_coord(x1, y1)
+  let p1 = convert_to_draw(x1, y1)
+  let p2 = convert_to_draw(x2, y2)
+  ctx.moveTo(p1.x, p1.y)
+  ctx.lineTo(p2.x, p2.y)
 }
 
 function noLoop(){
