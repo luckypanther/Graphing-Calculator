582f46f88e800d0628a4134abc926b082b293014
Sreyas Adiraju <luckypanther720@gmail.com>
2018-02-20 18:58:21 -0500

    Added settings panel to adjust parameters, still need to implement function parsing and generation.

--- code.js
+++ code.js
@@ -8,9 +8,13 @@ let x_min = -10
 let x_max = 10
 let y_min = -10
 let y_max = 10
+let tick_mark_height = 0.4
+let tick_mark_thickness = 0.2
+let x_ticks = 1
+let y_ticks = 1
 let CIRCLE_FILL = "black"
 let POINT_RADIUS = 1
-let LINE_WEIGHT = 2
+let LINE_WEIGHT = 2.5
 
 function init(){
   canvas = document.getElementById("canvas")
@@ -24,33 +28,75 @@ function draw(){
   if(!loop){
     return
   }
+  clear()
+  let func_string = document.getElementById("function").value
+  let f = generate(func_string)
+  x_min = parseFloat(document.getElementById("x-min").value)
+  x_max = parseFloat(document.getElementById("x-max").value)
+  y_min = parseFloat(document.getElementById("y-min").value)
+  y_max = parseFloat(document.getElementById("y-max").value)
+  tick_marks = document.getElementById("tick-marks").checked
+  x_ticks = parseFloat(document.getElementById("x-ticks").value)
+  y_ticks = parseFloat(document.getElementById("y-ticks").value)
   drawAxes()
-  drawGraph(f)
-  noLoop()
+  if(tick_marks){
+    drawTickMarks()
+  }
+  if(f){
+    drawGraph(f)
+  }  
 }
 
 function drawAxes(){
-  let top = convert_to_draw(0, y_max)
-  let bottom = convert_to_draw(0, y_min)
-  let right = convert_to_draw(x_max, 0)
-  let left = convert_to_draw(x_min, 0)
-  line(top.x, top.y, bottom.x, bottom.y)
-  line(left.x, left.y, right.x, right.y)
+  // let top = convert_to_draw(0, y_max)
+  // let bottom = convert_to_draw(0, y_min)
+  // let right = convert_to_draw(x_max, 0)
+  // let left = convert_to_draw(x_min, 0)
+  conv_line(0, y_max, 0, y_min)
+  conv_line(x_min, 0, x_max, 0)
+}
+
+function drawTickMarks(){
+  let x_low = Math.ceil(x_min/x_ticks)*x_ticks
+  // let x_high = Math.floor(x_max/x_ticks)*x_ticks
+  let y_low = Math.ceil(y_min/y_ticks)*y_ticks
+  // let y_high = Math.floor(y_max/y_ticks)*y_ticks
+  for(let i = x_low; i < x_max; i += x_ticks){
+    conv_line(i, -tick_mark_height, i, tick_mark_height, tick_mark_thickness)
+  }
+  for(let i = y_low; i < y_max; i += y_ticks){
+    conv_line(-tick_mark_height, i, tick_mark_height, i, tick_mark_thickness)
+  }
 }
 
 function drawGraph(func){
   let x_start = convert_to_graph(0, 0).x
   let y_start = func(x_start)
-  let current_p = convert_to_draw(x_start, y_start)
+  // let current_p = convert_to_draw(x_start, y_start)
   for(let i = 0; i < width; i++){
     let x = convert_to_graph(i, 0).x
     let y = func(x)
-    let p = convert_to_draw(x, y)
-    point(p.x, p.y)
-    line(current_p.x, current_p.y, p.x, p.y)
-    current_p = p
-    // console.log(p)
+    // let p = convert_to_draw(x, y)
+    // let d_squared = (current_p.x-p.x)*(current_p.x-p.x) + (current_p.y-p.y)*(current_p.y-p.y)
+    // let w = LINE_WEIGHT+Math.pow(d_squared, 0.85)*0.00004
+    // point(p.x, p.y)
+    // line(current_p.x, current_p.y, p.x, p.y)
+    conv_point(x, y)
+    conv_line(x_start, y_start, x, y)
+    // current_p = p
+    x_start = x
+    y_start = y
   }
+}
+
+function generate(func_string){
+  // func_string = func_string.replace(/\s/g, "")
+  let f = (x) => {}
+  if(func_string.charAt(0) == 'x' && func_string.charAt(1) == '^'){
+    f = (x) => Math.pow(x, parseInt(func_string.substring(2)))
+    return f
+  }
+  return demo  
 }
 
 function loadPixels(){
@@ -61,7 +107,7 @@ function setPixels(){
   ctx.putImageData(pixels, 0, 0, width, height);
 }
 
-function f(x){
+function demo(x){
   return x + Math.sin(x)
 }
 
@@ -88,17 +134,54 @@ function circle(x, y, r){
   ctx.fill()
 }
 
+function conv_point(x, y){
+  let p = convert_to_draw(x, y)
+  point(p.x, p.y)
+}
+
+function conv_line(x1, y1, x2, y2){
+  let p1 = convert_to_draw(x1, y1)
+  let p2 = convert_to_draw(x2, y2)
+  line(p1.x, p1.y, p2.x, p2.y)
+}
+
 function point(x, y){
   circle(x, y, POINT_RADIUS)
 }
 
-function line(x1, y1, x2, y2){
-  ctx.lineWidth = LINE_WEIGHT
+function line(x1, y1, x2, y2, width){
+  if(width){
+    ctx.lineWidth = width
+  }
+  else {
+    ctx.lineWidth = LINE_WEIGHT
+  }
   ctx.moveTo(x1, y1)
   ctx.lineTo(x2, y2)
   ctx.stroke()
+  ctx.lineWidth = 0
 }
 
 function noLoop(){
   loop = false
+}
+
+function reloop(){
+  loop = true
+}
+
+function clear(){
+  ctx.fillStyle = "white"
+  ctx.rect(0, 0, width, height)
+  ctx.fill()
+}
+
+function clearGraph(){
+  clear()
+  drawAxes()
+}
+
+function clearAxes(){
+  clear()
+  drawGraph(f)
 }
